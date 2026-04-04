73aca0784e1cf23bff771a2eb02918a56dd798ba
Sunil Kumar D O <sunilkumardo2004@gmail.com>
2026-04-05 00:13:30 +0530

    Added 3Sum solution (Two Pointer)

---
+++ Arrays/Threesum.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class Threesum {
+
+    /**
+     * Function to find all unique triplets that sum to zero
+     */
+    public static List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (nums == null || nums.length < 3) {
+            return result;
+        }
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+
+                    left++;
+                    right--;
+
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+
+        List<List<Integer>> result = threeSum(nums);
+
+        System.out.println("Triplets that sum to 0:");
+        for (List<Integer> triplet : result) {
+            System.out.println(triplet);
+        }
+    }
+}
